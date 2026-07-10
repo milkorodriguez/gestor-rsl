ea136e245a858f60dbd027bef8c01befda87912d
Milko Rodriguez <milko.rodriguez@pucp.pe>
2026-07-10 13:07:06 -0500

    ALB health check apunta a /api/dashboard

--- iac/modules/compute/main.tf
+++ iac/modules/compute/main.tf
@@ -114,7 +114,7 @@ resource "aws_lb_target_group" "tg_gestor_rsl" {
   target_type = "ip"
 
   health_check {
-    path                = "/actuator/health"
+    path                = "/api/dashboard"
     interval            = 30
     timeout             = 5
     healthy_threshold   = 2
